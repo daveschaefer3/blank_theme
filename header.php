38e7a70e6f62955d683fbfa8af8a0eb0e31a3dbb
Dave Schaefer <daveschaefer3@gmail.com>
2013-05-22 17:23:57 -0700

    initial commit
    files are mostly all blank. Chapter 5 in the ThemeShaper 2.0 tutorial

---
+++ header.php
@@ -0,0 +1,6 @@
+<div id="page" class="hfeed site">
+     <header id="masthead" class="site-header" role="banner">
+         <hgroup></hgroup>
+         <nav role="navigation" class="site-navigation main-navigation"></nav><!-- .site-navigation .main-navigation -->
+     </header><!-- #masthead .site-header -->
+<div id="main" class="site-main">
