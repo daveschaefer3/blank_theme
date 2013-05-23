972e6ef959fad32d4fdbfee8a23b7655618634c0
Dave Schaefer <daveschaefer3@gmail.com>
2013-05-22 22:55:43 -0700

    chapter 6 commit
    added content to functions.php, template-tag.php and tweaks.php

---
+++ inc/template-tags.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Custom template tags for this theme.
+ *
+ * Eventually, some of the functionality here could be replaced by core features
+ *
+ * @package Shape
+ * @since Shape 1.0
+ */
